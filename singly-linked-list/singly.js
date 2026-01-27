275df1761c3f2ae44f18caecd24c64dadd9b4c90
Risingdell <dhanush718718m@gmail.com>
2026-01-27 23:07:31 +0530

    complete-singly-linked-list

--- singly-linked-list/singly.js
+++ singly-linked-list/singly.js
@@ -86,20 +86,120 @@ class singlylinkedlist{
         if(!this.head){
             this.head=newnode;
             this.tail=newnode;
-        }
+        }else{                                                                                  
         let current=this.head;
         this.head=newnode;
         this.head.next=current;
+        }
         this.length++;
-        console.log('the new head is the ',this.head,'and the neaxt of the head is the ',this.head.next)
+        console.log('the new head is the ',this.head,'and the neaxt of the head is the ',this.head.next);
+        return this;
+    }
+    get(index){
+        
+        if(index<0 || index>=this.length){
+            return null
+        }
+        let count=0;
+        let current=this.head;
+        while(count!==index){
+            current=current.next;
+            count++;
+
+        }
+        return current;
+    }
+    set(index,value){
+        let replace=this.get(index);
+        if(!replace){
+            return false;
+        }else{
+            replace.value=value;
+        };
+        return replace.val;
+    }
+    insert(index,value){
+        if(index<0 || index>this.length){
+            return false;
+        }else if(index==this.length){
+            this.push(value);
+
+        }else if(index==0){
+            this.unshift(value);
+
+        }
+        let newnode=new Node(value);
+        let previousNode=this.get(index-1);
+
+        newnode.next=previousNode.next;
+        previousNode.next=newnode;
+        this.length++;
+        return true;
+
     }
 
-    
+    display(){
+        let len=0;
+        let current=this.head;
+    if(len=0){
+        return null;
+    }else{
+        while(len<this.length){
+            console.log(current.value);
+            current=current.next;
+            len++;
+
+
+        }
+    }
+}
+// remove(index){
+//     if(index<0 || index>=this.length){
+//         return false;
+//     }else if(index===0){
+//         return this.shift();
+//     }else if(index===this.length-1){
+//         return this.pop();
+//     }
+//     let ele= this.get(index-1);
+//     let pastEle=this.get(index+1);
+//     let pastEle1=this.get(index);
+//     console.log(ele);
+//     console.log(pastEle);
+//     console.log(pastEle1);
+//     ele.next=pastEle;
+//     this.length--;
+
+// }
+remove(index){
+    if(index<0 || index>=this.length){
+        return false;
+    }else if(index===0){
+        return this.shift();
+    }else if(index===this.length-1){
+        return this.pop();
+    }
+    let previousNode=this.get (index-1);
+    let removed= previousNode.next;
+    previousNode.next=removed.next;
+    this.length--;
+    return removed.value;
+
+}
 }
 let first=new singlylinkedlist();
 first.push("dhanush");
 first.push("Devansh");
-first.push("Dhruva")
-first.unshift("Divya");
+first.push("Dhruva");
+first.push("Divya");
+first.push("priya");
+first.push("amma");
 
+// console.log(first.get(2));
+// first.set(0,"devanush");4
+// // console.log(first.insert(2,"Devnsh"));
+console.log(first.remove(3));
+
+// first.insert(2,"Devnsh");
+first.display();
 
