44c564cdbc44cc8c53e701b948c8b8fca3de7513
Risingdell <dhanush718718m@gmail.com>
2026-02-09 23:37:22 +0530

    added bst

---
+++ JS_BASICS/BINARY SEARCH TREE/108. Convert Sorted Array to Binary Search Tree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    nums.sort((a,b)=>a-b);
+    console.log(nums);
+    
+};
+console.log(sortedArrayToBST([-10,-3,0,5,9]))
---
+++ JS_BASICS/twosum.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    
+    // let set= new Set();
+    let arr=new Array();
+    for(let i=0;i<root.length;i++){
+        arr[i]=root.val
+        
+    }
+    con
+    
+    
+};
+console.log(findTarget([5,3,6,2,4,null,7],9));
